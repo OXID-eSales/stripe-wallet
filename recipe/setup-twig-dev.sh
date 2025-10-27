8c3dc1360ee9472e73ca034da622b1882466fa09
Daniil Tkachev <daniil.tkachev@oxid-esales.com>
2025-10-27 14:21:16 +0100

    STRP-52 Update install recipe script

--- recipe/setup-twig-dev.sh
+++ recipe/setup-twig-dev.sh
@@ -48,23 +48,16 @@ cd "$PROJECT_ROOT" || exit 1
 
 $MODULE_ROOT/recipe/parts/shared/prepare_shop_package.sh -e"${edition}" -b"${branch}" || exit 1
 $MODULE_ROOT/recipe/parts/shared/require_twig_components.sh -e"${edition}" -b"${branch}"
-$MODULE_ROOT/recipe/parts/shared/require_theme_dev.sh -t"apex" -b"v3.0.0" -e"${edition}"
+$MODULE_ROOT/recipe/parts/shared/require_theme_dev.sh -t"apex" -b"v3.0.0"
 $MODULE_ROOT/recipe/parts/shared/require_demodata_package.sh -e"${edition}" -b"${branch}"
 
 mkdir -p "$PROJECT_ROOT"/source/extensions || exit 1
 cp -r "$MODULE_ROOT" "$PROJECT_ROOT"/source/extensions/stripe || exit 1
 
 mkdir -p ./source/var/configuration/environment/shops/1/modules
-cp $MODULE_ROOT/recipe/environment/1.yaml ./source/var/configuration/environment/shops/1/modules/stripe.yaml
 
 
-echo "composer require symfony/dotenv"
-docker compose exec -T php composer require symfony/dotenv --no-interaction --no-update
-
-echo "composer update"
-docker compose exec -T php composer update --no-interaction
-
-$PROJECT_ROOT/source/extensions/stripe/recipe/parts/shared/setup_database.sh
+$PROJECT_ROOT/source/extensions/stripe/recipe/parts/shared/setup_database.sh -e"${edition}" -b"${branch}"
 
 
 # Configure module in composer
@@ -72,9 +65,6 @@ docker compose exec -T \
   php composer config repositories.oxid-esales/stripe \
   --json '{"type":"path", "url":"./extensions/stripe", "options": {"symlink": true}}' || exit 1
 
-
-
-make up
 # Install all preconfigured dependencies
 docker compose exec -T php composer update --no-interaction
 docker compose exec -T php bin/oe-console oe:setup:demodata
@@ -82,7 +72,7 @@ docker compose exec -T php bin/oe-console oe:theme:activate apex
 docker compose exec -T php bin/oe-console oe:module:install extensions/stripe
 docker compose exec -T php bin/oe-console oe:module:activate stripe
 
-$PROJECT_ROOT/source/extensions/paypal/recipe/parts/shared/create_admin.sh
+$PROJECT_ROOT/source/extensions/stripe/recipe/parts/shared/create_admin.sh
 # Register all related project packages git repositories
 
 echo -e "\033[1;37m\033[1;42mInstallation is finished!\033[0m\n"
