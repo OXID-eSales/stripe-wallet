ce2f6e96e577df3a2fca0eea8ed1b7b864e866ed
Daniil Tkachev <daniil.tkachev@oxid-esales.com>
2025-11-04 13:38:59 +0100

    STRP-64 Update gha

--- .github/scripts/setup-autoload.sh
+++ .github/scripts/setup-autoload.sh
@@ -1,29 +1,43 @@
 #!/bin/bash
 set -e
 
+# Determine the correct test path (CI uses test-module, local uses extensions/stripe)
+if [ -d "./test-module" ]; then
+    TEST_PATH="./test-module/tests"
+elif [ -d "./extensions/stripe" ]; then
+    TEST_PATH="./extensions/stripe/tests"
+else
+    echo "Error: Cannot find module directory"
+    exit 1
+fi
+
+echo "Using test path: $TEST_PATH"
+
 # Add module test namespace to root composer.json autoload-dev
-php -r '
-$file = "composer.json";
-$json = json_decode(file_get_contents($file), true);
+php -r "
+\$file = 'composer.json';
+\$json = json_decode(file_get_contents(\$file), true);
 
 // Initialize autoload-dev structure if needed
-if (!isset($json["autoload-dev"])) {
-    $json["autoload-dev"] = [];
+if (!isset(\$json['autoload-dev'])) {
+    \$json['autoload-dev'] = [];
 }
-if (!isset($json["autoload-dev"]["psr-4"])) {
-    $json["autoload-dev"]["psr-4"] = [];
+if (!isset(\$json['autoload-dev']['psr-4'])) {
+    \$json['autoload-dev']['psr-4'] = [];
 }
 
 // Add the test namespace (only if not already present)
-$namespace = "OxidSolutionCatalysts\\Payments\\Tests\\";
-if (!isset($json["autoload-dev"]["psr-4"][$namespace])) {
-    $json["autoload-dev"]["psr-4"][$namespace] = "./extensions/stripe/tests";
-    file_put_contents($file, json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . PHP_EOL);
-    echo "Added test namespace to composer.json\n";
+\$namespace = 'OxidSolutionCatalysts\\\\Payments\\\\Tests\\\\';
+\$testPath = '$TEST_PATH';
+
+if (!isset(\$json['autoload-dev']['psr-4'][\$namespace])) {
+    \$json['autoload-dev']['psr-4'][\$namespace] = \$testPath;
+    file_put_contents(\$file, json_encode(\$json, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . PHP_EOL);
+    echo \"Added test namespace to composer.json with path: \$testPath\n\";
 } else {
-    echo "Test namespace already configured\n";
+    echo \"Test namespace already configured\n\";
 }
-'
+"
 
 # Regenerate autoloader
 composer dump-autoload
